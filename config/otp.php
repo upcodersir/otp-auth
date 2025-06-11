42083a9d714be8735807046816a84391541b290a
upcodersir <arghiani@gmail.com>
2025-06-11 09:30:55 +0330

    add mobile_containing_table

--- config/otp.php
+++ config/otp.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-  'valid_time' => 5,
+  'valid_time' => 5, // in minutes
   'user_table' => env('OTP_USER_TABLE', 'users'), // changable by end user
   'token_table' => 'tokens',
   // 'sms_client' => \Upcodersir\OtpAuth\Services\SmsProviders\ApiKeySmsProvider::class,
   'sms_client' => \Upcodersir\OtpAuth\Services\SmsProviders\ApiKeySmsProvider::class,
   'sms_api_url' => '',
   'sms_api_key' => '',
+  'mobile_containing_table' => '',
   // other options...
 ];
